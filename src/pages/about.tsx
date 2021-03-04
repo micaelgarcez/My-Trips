b021705fef267d5dc925710292c49b42defbb5fc
Micael Garcez <micaelgarcez@outlook.com>
2021-03-04 20:43:52 -0300

    Alter about template for page template, alter query for accept variable and use getStaticPaths in about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,25 @@
 import client from 'graphql/client'
 import { GET_PAGES } from 'graphql/queries'
-import AboutTemplate from 'templates/About'
+import { useRouter } from 'next/dist/client/router'
+import PageTemaplate from 'templates/Pages'
 
 export default function AboutPage() {
-  return <AboutTemplate />
+  const router = useRouter()
+
+  // retorna um loading, qq coisa enquanto está sendo criado
+  if (router.isFallback) return null
+
+  return <PageTemaplate />
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  return { paths, fallback: true }
 }
 
 export const getStaticProps = async () => {
